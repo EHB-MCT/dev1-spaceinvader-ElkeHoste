f560968ad0f32e48adaa803eeaee3cc1f6ea0511
Elke Hoste <elke.hoste@student.ehb.be>
2022-10-14 20:03:15 +0200

    create black rect with bright green rects

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,37 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+context.fillStyle='black';
+context.fillRect(0,0,300,300);
+
+context.fillStyle='#AAFF00';
+context.fillRect(60,50,50,50);
+
+context.fillStyle='#AAFF00';
+context.fillRect(30,50,50,50);
+
+
+context.fillStyle='#AAFF00';
+context.fillRect(120,110,50,50);
+
+context.fillStyle='#AAFF00';
+context.fillRect(170,50,50,50);
+
+context.fillStyle='#AAFF00';
+context.fillRect(40,200,50,50);
+
+context.fillStyle='#AAFF00';
+context.fillRect(90,200,50,50);
+
+context.fillStyle='#AAFF00';
+context.fillRect(145,200,50,50);
+
+context.fillStyle='#AAFF00';
+context.fillRect(200,200,50,50);
+
+
+
